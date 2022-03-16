07a161d7891521a34b2a64ce36049b64f861a41d
Claudio <claudio.capobianco@marconicloud.it>
2022-03-16 19:03:32 +0100

    Add comments

--- src/script.js
+++ src/script.js
@@ -10,6 +10,14 @@ let play = function (r, c) {
     console.log(`Hai premuto sulla casella: ${r} ${c}`);
     if (board[r][c] === '-') {
         board[r][c] = 'X';
+        console.log("Prima di inviare al server...");
+        console.log(board);
+        // send the board to the server
+        // update the web board on the client after receiving the response
+        // ...
+        console.log("Dopo la risposta del server...");
+        console.log(board);
+
     }
 }
 
